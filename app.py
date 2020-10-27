a55c598f56d8fabae2fd14d7b3046c27b822a640
tlebryk <43556997+tlebryk@users.noreply.github.com>
2020-10-27 19:04:19 -0400

    bottom bar light grey; new bottom frame with style

--- app.py
+++ app.py
@@ -12,21 +12,36 @@ class Flash:
 
     def __init__(self, root):
 
+
+        s = ttk.Style()
+        grey = '#D3D3D3'
+
+        s.configure("Frame1.TFrame", background = grey)
         mainframe = ttk.Frame(root, padding="3 3 12 12")
-        mainframe.grid(column=0, row=0, sticky= (tk.N, tk.W, tk.E, tk.S))
+        mainframe.grid(column=0, row=0)
+
+        #apologies for odd tk vs ttk choices (experimenting with both)
+        bottomframe = ttk.Frame(root, style="Frame1.TFrame")
+        bottomframe['padding']= (3,3,12,12)
+        bottomframe.grid(row = 100, column = 0, columnspan = 100, sticky = 'sew')
+        bottomframe.columnconfigure(0, weight = 5)
         root.columnconfigure(0, weight =1)
         root.rowconfigure(0,weight=1)
         root.title("Flashcard app")
 
-        Quiz = ttk.Label(mainframe, text = "Filler")
+        Quiz = ttk.Label(mainframe, text = "对吗?")
+        Quiz.grid(column=0, row=0)
+        # , anchor=tk.CENTER)
 
-        Quiz.grid(column=1, row = 1)
+        # Quiz.place(relx=0.5,rely=0.5, anchor=tk.CENTER)
+        # Quiz.grid(column=1, row = 1)
 
         # front = tk.Text(master=root, height = 10, width = 20)
 
-        flip = ttk.Button(mainframe, text = "flip card")
+        flip = ttk.Button(bottomframe, text = "flip card")
+        # flip.pack(side=tk.BOTTOM,)
         # flip.geometry("10x20")
-        flip.grid(column=1, row=2)
+        flip.grid(column=0, row=1)
 
 root = tk.Tk()
 Flash(root)
